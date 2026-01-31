2c3a08d5498730e3c43b8403eb40300c0d946936
Artebah <mafioznik445@gmail.com>
2026-01-31 22:28:22 +0200

    feat: adjust BoardsListItem layout for better responsiveness

--- client/src/components/BoardsList/BoardsListItem.tsx
+++ client/src/components/BoardsList/BoardsListItem.tsx
@@ -4,7 +4,7 @@ interface BoardsListItemProps extends IBoard {}
 
 function BoardsListItem({ createdAt, title }: BoardsListItemProps) {
   return (
-    <div className="card bg-base-100 w-96 shadow-sm">
+    <div className="card bg-base-100 max-w-96 shadow-sm">
       <figure>
         <img
           src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
--- client/src/components/BoardsList/index.tsx
+++ client/src/components/BoardsList/index.tsx
@@ -7,8 +7,12 @@ function BoardsList() {
   return (
     <div>
       {isLoading && <div>loading...</div>}
-      {boardsList &&
-        boardsList.map((board) => <BoardsListItem key={board.id} {...board} />)}
+      <div className="bg-gray-charcoal p-4 grid grid-cols-4 gap-4">
+        {boardsList &&
+          boardsList.map((board) => (
+            <BoardsListItem key={board.id} {...board} />
+          ))}
+      </div>
       {boardsList && boardsList.length === 0 && <div>No boards found</div>}
       {!boardsList && <div>Couldn't fetch your boards</div>}
     </div>
