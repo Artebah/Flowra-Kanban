2862b848d8a0001b7a038cac7a55f3bb5eb9af63
Artebah <mafioznik445@gmail.com>
2026-02-01 19:39:32 +0200

    feat: enhance logout functionality to prevent navigation to login on auth pages

--- client/src/hooks/useLogout.tsx
+++ client/src/hooks/useLogout.tsx
@@ -1,11 +1,15 @@
 import { useClearUser } from "../store/auth/selectors";
 import { useNavigate } from "react-router";
-import { routes } from "../constants/routes";
+import { AUTH_ROUTES, routes } from "../constants/routes";
 import React from "react";
 
 export const useLogout = () => {
   const clearUser = useClearUser();
   const navigate = useNavigate();
+  const isAuthPage = React.useMemo(
+    () => AUTH_ROUTES.includes(location.pathname),
+    []
+  );
 
   const logout = React.useCallback(() => {
     clearUser();
@@ -13,8 +17,10 @@ export const useLogout = () => {
     localStorage.removeItem("accessToken");
     localStorage.removeItem("refreshToken");
 
-    navigate(routes.login);
-  }, [clearUser, navigate]);
+    if (!isAuthPage) {
+      navigate(routes.login);
+    }
+  }, [clearUser, navigate, isAuthPage]);
 
   return logout;
 };
