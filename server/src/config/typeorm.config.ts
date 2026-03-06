7460c0f2d610b8135c5f946b6154a9ccbd57b2ff
Artebah <mafioznik445@gmail.com>
2026-03-06 21:24:07 +0200

    feat: add migrations configuration and refactor TypeORM setup

--- server/src/config/typeorm.config.ts
+++ server/src/config/typeorm.config.ts
@@ -1,16 +1,8 @@
 import { registerAs } from "@nestjs/config";
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { dataSourceOptions } from "src/db/data-source";
 
 export const typeormConfig = registerAs(
   "typeorm",
-  (): TypeOrmModuleOptions => ({
-    type: "postgres",
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    entities: [__dirname + "/../**/*.entity.{js,ts}"],
-    synchronize: true,
-  }),
+  (): TypeOrmModuleOptions => dataSourceOptions,
 );
