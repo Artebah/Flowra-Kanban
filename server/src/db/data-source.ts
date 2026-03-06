1301240d8116fbb867df5685775c6a9a9e70d127
Artebah <mafioznik445@gmail.com>
2026-03-06 22:30:29 +0200

    feat: update docker-compose and data source configuration for database host

--- server/src/db/data-source.ts
+++ server/src/db/data-source.ts
@@ -6,7 +6,7 @@ config({ path: join(__dirname, "../../../.env") });
 
 export const dataSourceOptions: DataSourceOptions = {
   type: "postgres",
-  host: process.env.DB_HOST === "db" ? "localhost" : process.env.DB_HOST,
+  host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
