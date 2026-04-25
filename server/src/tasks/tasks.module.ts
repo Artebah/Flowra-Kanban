3830286c0cae4fa0d47d7b064ca74a08c1ce8c90
Artebah <mafioznik445@gmail.com>
2026-04-25 16:36:24 +0300

    fix: fix error with not imported StorageModule and install tiptap deps

--- server/src/tasks/tasks.module.ts
+++ server/src/tasks/tasks.module.ts
@@ -5,10 +5,14 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { BoardMember } from "src/boards/entities/BoardMember.entity";
 import { Task } from "./entities/Task.entity";
 import { BoardColumn } from "src/columns/entities/Column.entity";
+import { StorageModule } from "src/storage/storage.module";
 
 @Module({
   controllers: [TasksController],
   providers: [TasksService],
-  imports: [TypeOrmModule.forFeature([BoardMember, Task, BoardColumn])],
+  imports: [
+    TypeOrmModule.forFeature([BoardMember, Task, BoardColumn]),
+    StorageModule,
+  ],
 })
 export class TasksModule {}
