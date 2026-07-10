47343d1d502c219114083734c11360e5c947a501
Artebah <mafioznik445@gmail.com>
2026-07-10 18:54:42 +0300

    feat: integrate LabelsService and add getAllLabels endpoint in BoardsController

--- server/src/boards/boards.controller.ts
+++ server/src/boards/boards.controller.ts
@@ -12,11 +12,16 @@ import { BoardsService } from "./boards.service";
 import { UserDecorator } from "src/auth/decorators/user.decorator";
 import { JwtPayload } from "src/auth/interfaces/jwt-payload.interface";
 import { JwtAuthGuard } from "src/auth/guards/jwt-auth.guard";
+import { LabelsService } from "src/labels/labels.service";
+import { BoardAccessGuard } from "src/common/guards/board-access.guard";
 
 @Controller("boards")
 @UseGuards(JwtAuthGuard)
 export class BoardsController {
-  constructor(private readonly boardsService: BoardsService) {}
+  constructor(
+    private readonly boardsService: BoardsService,
+    private readonly labelsService: LabelsService,
+  ) {}
 
   @Post()
   create(
@@ -26,6 +31,7 @@ export class BoardsController {
     return this.boardsService.create(createBoardDto, user.sub);
   }
 
+  @UseGuards(BoardAccessGuard)
   @Get("/:boardId")
   getCurrentBoard(
     @Param("boardId", new ParseUUIDPipe()) boardId: string,
@@ -34,8 +40,15 @@ export class BoardsController {
     return this.boardsService.getCurrentBoard(boardId, user.sub);
   }
 
+  @UseGuards(BoardAccessGuard)
   @Get()
   getAllMyBoards(@UserDecorator() user: JwtPayload) {
     return this.boardsService.getAllMyBoards(user.sub);
+  }
+
+  @UseGuards(BoardAccessGuard)
+  @Get("/:boardId/labels")
+  getAllLabels(@Param("boardId", new ParseUUIDPipe()) boardId: string) {
+    return this.labelsService.getAll(boardId);
   }
 }
--- server/src/boards/boards.module.ts
+++ server/src/boards/boards.module.ts
@@ -4,10 +4,11 @@ import { BoardsService } from "./boards.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Board } from "./entities/Board.entity";
 import { BoardMember } from "./entities/BoardMember.entity";
+import { LabelsModule } from "../labels/labels.module";
 
 @Module({
   controllers: [BoardsController],
   providers: [BoardsService],
-  imports: [TypeOrmModule.forFeature([Board, BoardMember])],
+  imports: [TypeOrmModule.forFeature([Board, BoardMember]), LabelsModule],
 })
 export class BoardsModule {}
