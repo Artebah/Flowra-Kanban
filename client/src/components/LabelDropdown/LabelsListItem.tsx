c08c7b03e75b1366ab9a3720bcc5508af60e3488
Artebah <mafioznik445@gmail.com>
2026-07-10 18:20:14 +0300

    refactor: improve structure and accessibility of LabelsListItem component

--- client/src/components/LabelDropdown/LabelsListItem.tsx
+++ client/src/components/LabelDropdown/LabelsListItem.tsx
@@ -4,10 +4,15 @@ import { Checkbox } from "../ui/checkbox";
 
 function LabelsListItem() {
   return (
-    <div className="flex gap-3 items-center">
-      <Checkbox />
-      <div className="bg-emerald-700 rounded-xs px-4 h-7 flex items-center">
-        <p className="text-sm">label 1</p>
+    <div className="flex gap-2 items-center">
+      <div className="flex grow items-center gap-2 cursor-pointer group">
+        <Checkbox id={"label1-check"} className="group-hover:brightness-110" />
+        <label
+          htmlFor="label1-check"
+          className="group-hover:brightness-110 grow bg-emerald-700 rounded-xs px-4 h-7 flex items-center"
+        >
+          <span className="text-sm">label 1</span>
+        </label>
       </div>
       <Button className="p-2 size-8">
         <Edit className="size-4!" />
