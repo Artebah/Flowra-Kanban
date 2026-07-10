28dff749a71fd2b838edc2c7a84c260fc2f2199e
Artebah <mafioznik445@gmail.com>
2026-07-10 18:21:17 +0300

    fix: adjust cursor pointer styling for improved accessibility in LabelsListItem component

--- client/src/components/LabelDropdown/LabelsListItem.tsx
+++ client/src/components/LabelDropdown/LabelsListItem.tsx
@@ -5,11 +5,14 @@ import { Checkbox } from "../ui/checkbox";
 function LabelsListItem() {
   return (
     <div className="flex gap-2 items-center">
-      <div className="flex grow items-center gap-2 cursor-pointer group">
-        <Checkbox id={"label1-check"} className="group-hover:brightness-110" />
+      <div className="flex grow items-center gap-2 group">
+        <Checkbox
+          id={"label1-check"}
+          className="cursor-pointer group-hover:brightness-110"
+        />
         <label
           htmlFor="label1-check"
-          className="group-hover:brightness-110 grow bg-emerald-700 rounded-xs px-4 h-7 flex items-center"
+          className="cursor-pointer group-hover:brightness-110 grow bg-emerald-700 rounded-xs px-4 h-7 flex items-center"
         >
           <span className="text-sm">label 1</span>
         </label>
