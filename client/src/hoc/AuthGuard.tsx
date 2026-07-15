dc200c006c6551711224395df422e821c9cfb9c1
Artebah <mafioznik445@gmail.com>
2026-07-15 17:54:36 +0300

    feat: remove unused logout functionality and related error handling in AuthGuard

--- client/src/hoc/AuthGuard.tsx
+++ client/src/hoc/AuthGuard.tsx
@@ -2,26 +2,15 @@ import React from "react";
 import { useFetchMe } from "../hooks/api/auth/useFetchMe";
 import { isPrivate, routes } from "../constants/routes";
 import { useNavigate } from "react-router";
-import { AxiosError } from "axios";
-import { useLogout } from "../hooks/useLogout";
 import { useSetUser } from "../store/auth/selectors";
 import { useIsAuthPage } from "../hooks/useIsAuthPage";
 
 function AuthGuard({ children }: React.PropsWithChildren) {
-  const { data, isError, error, isLoading } = useFetchMe();
+  const { data, isLoading } = useFetchMe();
   const navigate = useNavigate();
 
   const isAuthPage = useIsAuthPage();
   const setUser = useSetUser();
-  const logout = useLogout();
-
-  React.useEffect(() => {
-    if (isError) {
-      if (error instanceof AxiosError && error.status === 401) {
-        logout();
-      }
-    }
-  }, [isError, error, logout]);
 
   React.useEffect(() => {
     if (data) {
