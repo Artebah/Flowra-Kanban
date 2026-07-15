c459119a314c3f84c60b645a2302d64b4c3f67b7
Artebah <mafioznik445@gmail.com>
2026-07-15 15:12:24 +0300

    feat: add delete board functionality with confirmation dialog and dropdown menu

--- client/src/pages/BoardPage.tsx
+++ client/src/pages/BoardPage.tsx
@@ -10,12 +10,23 @@ import { useGetAllTasks } from "../hooks/api/tasks/useGetAllTasks";
 import EditableText from "@/components/EditableText";
 import { useUpdateBoard } from "@/hooks/api/boards/useUpdateBoard";
 import { cn } from "@/lib/utils";
+import Button from "@/components/Button";
+import { MoreHorizontal, TrashIcon } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Dialog, DialogContent } from "@/components/ui/dialog";
+import { useDeleteBoard } from "@/hooks/api/boards/useDeleteBoard";
 
 function BoardPage() {
   const params = useParams();
   const navigate = useNavigate();
   const setColumns = useSetColumns();
   const setTasksByColumn = useSetTasksByColumn();
+  const [openDeleteBoardModal, setOpenDeleteBoardModal] = React.useState(false);
 
   const boardId = params.id!;
 
@@ -28,6 +39,7 @@ function BoardPage() {
   } = useBoardById(params.id!);
 
   const updateBoard = useUpdateBoard();
+  const deleteBoard = useDeleteBoard();
 
   const { data: tasks, isLoading: isLoadingTasks } = useGetAllTasks(boardId);
 
@@ -52,6 +64,10 @@ function BoardPage() {
     }
   };
 
+  const onDeleteBoard = () => {
+    deleteBoard.mutate(boardId, { onSuccess: () => {} });
+  };
+
   if (isLoadingBoard || isLoadingColumns || isLoadingTasks) {
     return (
       <div className="pt-4 pb-4">
@@ -71,21 +87,68 @@ function BoardPage() {
   } else if (boardData) {
     return (
       <div className="min-h-[calc(100vh-80px)] flex flex-col gap-3">
-        <div className="h-14 flex items-center pl-7 bg-black/25">
-          <EditableText
-            containerClassName={cn(" flex-0! min-w-60", {
-              "hover:bg-white/10 rounded-md px-3! whitespace-nowrap truncate":
-                !isEditableTitle,
-            })}
-            inputClassName="flex-0! min-w-60"
-            isEditable={isEditableTitle}
-            setIsEditable={setIsEditableTitle}
-            onSave={(title) => onUpdateBoard({ title })}
-            text={boardData.board.title}
-            disabled={updateBoard.isPending}
-          />
+        <div className="h-14 flex items-center justify-between px-7 bg-black/25">
+          <div>
+            <EditableText
+              containerClassName={cn("min-w-60 px-3!", {
+                "hover:bg-white/10 rounded-md whitespace-nowrap truncate":
+                  !isEditableTitle,
+              })}
+              inputClassName="min-w-60"
+              isEditable={isEditableTitle}
+              setIsEditable={setIsEditableTitle}
+              onSave={(title) => onUpdateBoard({ title })}
+              text={boardData.board.title}
+              disabled={updateBoard.isPending}
+            />
+          </div>
+
+          <div className="grow flex gap-3 justify-end">
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <Button className="rounded-full" isIconOnly>
+                  <MoreHorizontal />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="min-w-[180px]">
+                <DropdownMenuItem
+                  onClick={() => setOpenDeleteBoardModal(true)}
+                  className="text-red-500 hover:text-red-500! flex gap-3 items-center!"
+                >
+                  <TrashIcon className="size-4" />
+                  <p className="leading-0">Delete board</p>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
         </div>
         <BoardLayout boardId={boardId} />
+
+        <Dialog
+          open={openDeleteBoardModal}
+          onOpenChange={(o) => !o && setOpenDeleteBoardModal(false)}
+        >
+          <DialogContent className="bg-gray-charcoal">
+            <h2 className="text-lg font-semibold mb-4">
+              Are you sure you want to delete board "{boardData.board.title}"?
+            </h2>
+            <div className="flex justify-end gap-2">
+              <Button
+                onClick={onDeleteBoard}
+                variant="default"
+                className="bg-red-500 hover:bg-red-600!"
+              >
+                Delete
+              </Button>
+              <Button
+                variant="outline"
+                onClick={() => setOpenDeleteBoardModal(false)}
+              >
+                Cancel
+              </Button>
+            </div>
+          </DialogContent>
+        </Dialog>
       </div>
     );
   }
