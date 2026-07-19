5ab62cf7fb1a2893a9401bc17e0dc1e1fb804fbd
Artebah <mafioznik445@gmail.com>
2026-07-19 11:55:48 +0300

    fix: update CalendarDayButton styling for selected state to use rounded-full

--- client/src/components/ui/calendar.tsx
+++ client/src/components/ui/calendar.tsx
@@ -117,7 +117,7 @@ function Calendar({
           defaultClassNames.range_end
         ),
         today: cn(
-          "rounded-(--cell-radius) bg-muted text-foreground data-[selected=true]:rounded-none",
+          "rounded-(--cell-radius) bg-muted text-foreground data-[selected=true]:rounded-full",
           defaultClassNames.today
         ),
         outside: cn(
