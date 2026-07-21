8e3ccb931c8775eb518e84b507ab0ab92be3e8a2
Artebah <mafioznik445@gmail.com>
2026-07-21 11:24:38 +0300

    feat: add member assignment functionality to MembersDropdown component

--- client/src/components/MembersDropdown/index.tsx
+++ client/src/components/MembersDropdown/index.tsx
@@ -5,6 +5,7 @@ import { useModalDetailsData } from "@/store/kanban/selectors";
 import Button from "../Button";
 import { XIcon } from "lucide-react";
 import { useGetBoardMembers } from "@/hooks/api/boards/useGetBoardMembers";
+import { useAssignMembers } from "@/hooks/api/tasks/useAssignMembers";
 
 interface MembersDropdownProps {
   triggerRender: React.ReactElement;
@@ -20,11 +21,25 @@ function MembersDropdown({ triggerRender }: MembersDropdownProps) {
       assignedMembers.some((assignedMember) => assignedMember.id === memberId),
     [assignedMembers]
   );
+  const assignMembers = useAssignMembers();
+
+  const onClickMember = (memberId: string) => {
+    if (!boardId || !taskId) return;
+
+    const ids = assignedMembers.map((assignedMember) => assignedMember.id);
+    const isAssigned = ids.includes(memberId);
+
+    const membersIds = isAssigned
+      ? ids.filter((id) => id !== memberId)
+      : [...ids, memberId];
+
+    assignMembers.mutate({ boardId, taskId, dto: { membersIds } });
+  };
 
   return (
     <Popover title="Members" triggerRender={triggerRender}>
       {boardMembers.map((boardMember) => (
-        <Button>
+        <Button onClick={() => onClickMember(boardMember.id)}>
           <div className="grow flex items-center gap-3">
             <div className="size-8 rounded-full overflow-hidden">
               <img
