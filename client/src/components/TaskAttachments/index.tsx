fef91278da794b91ca6876f5beaa4a2724d1e231
Artebah <mafioznik445@gmail.com>
2026-07-22 18:23:48 +0300

    feat: refactor TaskAttachments component to include columnId and improve file input handling

--- client/src/components/TaskAttachments/index.tsx
+++ client/src/components/TaskAttachments/index.tsx
@@ -2,29 +2,48 @@ import { useGetAttachments } from "@/hooks/api/tasks/useGetAttachments";
 import TaskAttachmentsItem from "./TaskAttachmentsItem";
 import { Paperclip } from "lucide-react";
 import Button from "../Button";
+import { useAttachImagesToTask } from "@/hooks/useAttachImagesToTask";
+import React from "react";
+import { TASK_ATTACHMENT_MAX_SIZE } from "@/constants/taskAttachmentMaxSize";
 
 interface TaskAttachmentsProps {
   boardId: string;
   taskId: string;
+  columnId: string;
 }
 
-function TaskAttachments({ boardId, taskId }: TaskAttachmentsProps) {
+function TaskAttachments({ boardId, taskId, columnId }: TaskAttachmentsProps) {
   const { data: attachments = [] } = useGetAttachments(boardId, taskId);
+  const inputRef = React.useRef<HTMLInputElement>(null);
+
+  const { onChange } = useAttachImagesToTask({ boardId, taskId, columnId });
 
   return (
-    <div className="mt-8 px-6 pb-8">
+    <div className="px-6 pb-8">
       <div className="flex justify-between gap-3 items-center">
         <div className="flex items-center gap-3">
           <Paperclip /> <p className="font-bold">Attachments</p>
         </div>
 
-        <Button className="h-8 leading-0" variant="outline">
+        <Button
+          onClick={() => inputRef.current?.click()}
+          className="h-8 leading-0"
+          variant="outline"
+        >
           Add
         </Button>
       </div>
+      <input
+        multiple
+        onChange={onChange}
+        ref={inputRef}
+        size={TASK_ATTACHMENT_MAX_SIZE}
+        type="file"
+        className="size-0 opacity-0"
+      />
 
       <div className="pl-9">
-        <p className="font-bold text-xs mt-4 text-gray-300">Files</p>
+        <p className="font-bold text-xs text-gray-300">Files</p>
 
         <div className="mt-3 flex flex-col gap-2">
           {attachments.map((attachment) => {
