7f21650d5c8665b79df2160230b73784a7f606cc
Artebah <mafioznik445@gmail.com>
2026-07-30 13:59:55 +0300

    feat: add coverUrl field to CreateBoardDto and update create method in BoardsService

--- server/src/boards/boards.service.ts
+++ server/src/boards/boards.service.ts
@@ -21,10 +21,13 @@ export class BoardsService {
     private boardMembersRepository: Repository<BoardMember>,
   ) {}
 
-  create({ title }: CreateBoardDto, userId: string): Promise<Board> {
+  create({ title, coverUrl }: CreateBoardDto, userId: string): Promise<Board> {
     return this.boardsRepository.manager.transaction(
       async (transactionalEntityManager) => {
-        const board = transactionalEntityManager.create(Board, { title });
+        const board = transactionalEntityManager.create(Board, {
+          title,
+          coverUrl,
+        });
 
         await transactionalEntityManager.save(board);
 
